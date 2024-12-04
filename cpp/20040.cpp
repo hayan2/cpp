72df69269eba74ddebf86b890e028cb2f09bb237
hayan2 <uhayan2@gmail.com>
2024-12-04 18:57:00 +0900

    1204

---
+++ cpp/20040.cpp
@@ -0,0 +1,45 @@
+#define MAX_LEN 500001
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int cache[MAX_LEN];
+int N, M;
+
+int find(int x) {
+	if (cache[x] == x) return x;
+	else return cache[x] = find(cache[x]);
+}
+
+void unionSet(int a, int b) {
+	a = find(a);
+	b = find(b);
+
+	if (a < b) cache[b] = a;
+	else if (a > b) cache[a] = b;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int u, v, res = 0;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) cache[i] = i;
+	
+	for (int i = 1; i <= M; i++) {
+		cin >> u >> v;
+
+		if (find(u) == find(v)) {
+			res = i;
+			break;
+		}
+		unionSet(u, v);
+	}
+
+	cout << res;
+
+	return 0;
+}
