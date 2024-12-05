2838dbac253454dc5d1e1dd7fc0fbc596cf91b75
hayan2 <uhayan2@gmail.com>
2024-12-05 17:18:33 +0900

    1205

---
+++ cpp/1197.cpp
@@ -0,0 +1,66 @@
+#define MAX_LEN 10001
+#define ull unsigned long long
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, pair<int, int>> type;
+
+vector<type> cache;
+int root[MAX_LEN];
+int V, E;
+
+int find(int x) {
+	if (root[x] == x) return root[x];
+	else return root[x] = find(root[x]);
+}
+
+void unionSet(int a, int b) {
+	a = find(a);
+	b = find(b);
+
+	if (a > b) root[b] = a;
+	else if (a < b) root[a] = b;
+}
+
+int solved() {
+	vector<type> mst;
+	int ret = 0;
+
+	for (type cur : cache) {
+		int u = cur.second.first;
+		int v = cur.second.second;
+
+		if (find(u) == find(v)) continue;
+
+		mst.push_back(cur);
+		unionSet(u, v);
+		ret += cur.first;
+
+		if (mst.size() == V - 1) return ret;
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> V >> E;	
+	int u, v, w;
+
+	while (E--) {
+		cin >> u >> v >> w;
+
+		cache.push_back({ w, { u, v } });
+	}
+
+	for (int i = 1; i <= V; i++) root[i] = i;
+	sort(cache.begin(), cache.end());
+
+	cout << solved();
+
+	return 0;
+}
