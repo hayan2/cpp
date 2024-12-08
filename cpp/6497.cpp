d91ab4aa79409a34783324c69d7ebf96918c5120
hayan2 <uhayan2@gmail.com>
2024-12-08 16:21:29 +0900

    1207

---
+++ cpp/6497.cpp
@@ -0,0 +1,80 @@
+#define MAX_LEN 200015
+#define ull unsigned long long
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+// AC  mem	9076KB    time	228MS
+
+using namespace std;
+
+typedef pair<ull, pair<int, int>> Mst;
+
+vector<Mst> cache;
+ull res = 0;
+int root[MAX_LEN];
+int N, M;
+
+int find(int x) {
+	if (root[x] == x) return root[x];
+	else return root[x] = find(root[x]);
+}
+
+void unionSet(int a, int b) {
+	a = find(a);
+	b = find(b);
+
+	if (a > b) root[b] = a;
+	else root[a] = b;
+}
+
+ull solved() {
+	ull ret = 0;
+	int cnt = 0;
+
+	for (Mst cur : cache) {
+		int u = cur.second.first;
+		int v = cur.second.second;
+		ull w = cur.first;
+
+		if (find(u) == find(v)) continue;
+
+		if (cnt == N - 1) break;
+		cnt++;
+		unionSet(u, v);
+		ret += w;
+	}
+
+	return ret;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	while (1) {
+		cin >> N >> M;
+		if (!N && !M) break;
+		for (int i = 0; i < N; i++) root[i] = i;
+
+		for (int i = 0; i < M; i++) {
+			ull w;
+			int u, v;
+
+			cin >> u >> v;
+			cin >> w;
+
+			cache.push_back({ w, { u, v } });
+			res += w;			
+		}
+		sort(cache.begin(), cache.end());
+
+		cout << res - solved() << "\n";
+
+		cache.clear();
+		res = 0;
+	}
+	
+	return 0;
+}
