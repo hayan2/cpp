28b73974c4a4ca9a72283df54e0294159e50f75e
hayan2 <uhayan2@gmail.com>
2025-01-02 19:16:56 +0900

    0102-1

--- cpp/2162.cpp
+++ cpp/2162.cpp
@@ -1,3 +1,4 @@
+#define ll long long
 #define MAX_LEN 3001
 #define VISITED 1
 #include <iostream>
@@ -7,8 +8,10 @@
 
 using namespace std;
 
+// WA
+
 typedef struct Coordinate {
-	int x, y;
+	ll x, y;
 	
 	void read() { cin >> x >> y; }
 }Coordinate;
@@ -28,36 +31,97 @@ void unionSet(int a, int b) {
 	a = find(a);
 	b = find(b);
 
-	if (a < b) {
+	if (a > b) {
 		root[b] = a;
-		cache[b] += cache[a];
-	}
-	else if (a > b) {
-		root[a] = b;
 		cache[a] += cache[b];
+		group -= cache[b];
+	}
+	else if (a < b) {
+		root[a] = b;
+		cache[b] += cache[a];
+		group -= cache[a];
 	}
 }
 
-int ccw(Coordinate p1, Coordinate p2, Coordinate p3) {
+ll ccw(Coordinate p1, Coordinate p2, Coordinate p3) {
 	return ((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y));
 }
 
-bool isOverlapped() {
+// ret 0 : p1 is farther from the origin
+// ret 1 : p2 is farther from the origin
+ll compareDistance(Coordinate p1, Coordinate p2) {
+	ll d1 = p1.x * p1.x + p1.y * p1.y;
+	ll d2 = p2.x * p2.x + p2.y * p2.y;
 
+	if (d1 > d2) return 0;
+	else return 1;
+}
+
+ll getDistance(Coordinate p1) {
+	return sqrt(p1.x * p1.x + p1.y * p1.y);
+}
+
+ll isBetween(Coordinate p1, Coordinate p2, Coordinate p3) {
+	return ((p1.x <= p3.x && p3.x <= p2.x) && (p1.y <= p3.y && p3.y <= p2.y)) || ((p1.x >= p3.x && p3.x >= p2.x) && (p1.y >= p3.y && p3.y >= p2.y));
+}
+
+bool isOverlapped() {
+	return true;
 }
 
 void solved(int s) {
 	for (int i = 0; i < N; i++) {
-		for (int j = i + 1; j < N; j++) {
+		for (int j = 0; j < N; j++) {
 			Coordinate p1 = line[i].first;
 			Coordinate p2 = line[i].second;
 			Coordinate p3 = line[j].first;
 			Coordinate p4 = line[j].second;
 
-			int l1 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
-			int l2 = ccw(p3, p4, p1) * ccw(p3, p4, p2);
+			ll abc = ccw(p1, p2, p3);
+			ll abd = ccw(p1, p2, p4);
+			ll cda = ccw(p3, p4, p1);
+			ll cdb = ccw(p3, p4, p2);
 
+			ll l1 = abc * abd;
+			ll l2 = cda * cdb;
 
+			if (i == j) continue;
+
+			if (l1 < 0 && l2 < 0) {
+				printf("%d, %d are overlapped\n", i, j);
+				unionSet(i, j);
+			}
+			else if (!l1 && !l2) {
+				// The four points lie on a straight line.
+				// (p1 < p4 && p2 > p4) (p1 < p3 && p2 > p3)
+				if (!compareDistance(p1, p3) && (isBetween(p1, p2, p3) || isBetween(p1, p2, p4))) {
+					printf("%d, %d are overlapped\n", i, j);
+					unionSet(i, j);
+				}
+				else if (compareDistance(p1, p3) && (isBetween(p3, p4, p2) || isBetween(p3, p4, p1))) {
+					printf("%d, %d are overlapped\n", i, j);
+					unionSet(i, j);
+				}
+				else {
+					printf("%d, %d are not overlapped\n", i, j);
+
+				}
+			}
+			else if (!l1 || !l2) {
+				// The three points lie on a straight line.
+				if (isBetween(p1, p2, p3) || isBetween(p1, p2, p4) || isBetween(p3, p4, p1) || isBetween(p3, p4, p2)) {
+					printf("%d, %d are overlapped\n", i, j);
+					unionSet(i, j);
+				}
+				else {
+					printf("%d, %d are not overlapped\n", i, j);
+
+				}
+			}
+			else {
+				printf("%d, %d are not overlapped\n", i, j);
+
+			}
 		}
 	}
 }
@@ -68,8 +132,9 @@ int main(void) {
 	cout.tie(NULL);
 
 	cin >> N;
-	group = N;	
-	for (int i = 0; i < MAX_LEN; i++) {
+	group = N;
+
+	for (int i = 0; i < N; i++) {
 		cache[i] = 1;
 		root[i] = i;
 	}
@@ -78,10 +143,15 @@ int main(void) {
 		Coordinate p1, p2;
 		
 		p1.read(), p2.read();
-		line.push_back({ p1, p2 });
+		if (compareDistance(p1, p2)) line.push_back({ p1, p2 });
+		else line.push_back({ p2, p1 });
 	}
-
+	
 	solved(0);
+
+	for (int i = 0; i < N; i++) cnt = (cnt > cache[i]) ? cnt : cache[i];
+
+	cout << group << "\n" << cnt;
 
 	return 0;
 }
