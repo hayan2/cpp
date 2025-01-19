f67d156da39aeaff0e08ff64c13bb287741d8710
hayan2 <uhayan2@gmail.com>
2025-01-19 22:17:36 +0900

    0119

--- cpp/2482.cpp
+++ cpp/2482.cpp
@@ -3,39 +3,31 @@
 
 #define MOD 1000000003
 #define MAX_LEN 1001
-#define VISITED 1
 
 using namespace std;
 
-int visited[MAX_LEN][MAX_LEN];
 int dp[MAX_LEN][MAX_LEN];
 int N, K;
-
-int solved() {
-	int cnt = 1, ret = 0;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = i + 2; j < N - 1; j++) {
-			cnt++;
-			if (cnt == K) {
-				ret++;
-				cnt = 1;
-			}
-		}
-	}
-	return ret;
-}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	memset(visited, 0, sizeof(visited));
-	
+		
 	cin >> N >> K;
 
-	cout << solved();
+	for (int i = 0; i <= N; i++) {
+		dp[i][0] = 1;
+		dp[i][1] = i;
+	}
+	dp[1][1] = 0;
+	for (int i = 2; i <= N; i++) {
+		for (int j = 2; j <= K; j++) {
+			dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % MOD;			
+		}		
+	}
+
+	cout << dp[N][K] % MOD;
 
 	return 0;
 }
