fa7d080a7160226e1092c7d3dabf12adf612cdf3
hayan2 <uhayan2@gmail.com>
2025-01-22 19:17:00 +0900

    0122-1

---
+++ cpp/5670.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+
+using namespace std;
+
+
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	return 0;
+}
