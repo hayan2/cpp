9f9e6a4df15323dc328e0446d08880496c5c71fb
hayan2 <uhayan2@gmail.com>
2025-01-29 16:07:43 +0900

    0129

---
+++ cpp/3665.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_LEN 501
+
+using namespace std;
+
+int cache[MAX_LEN][MAX_LEN];
+int T, n, m;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	return 0;
+}
