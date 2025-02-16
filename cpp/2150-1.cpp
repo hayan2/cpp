f9bb82b8e7eba10ce8f3df596f01545ce8875e5f
hayan2 <uhayan2@gmail.com>
2025-02-17 00:28:02 +0900

    0217

--- cpp/2150-1.cpp
+++ cpp/2150-1.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-#define MAX_V 10001
-#define FINISHED true
+#define MAX_V 10001 
+#define FINISHED 1
 
 vector<int> edge[MAX_V];
 vector<vector<int>> res;
 stack<int> s;
-int id[MAX_V] = { 0, };
 bool finished[MAX_V] = { 0, };
-int V, E, u, v, cnt = 0, idx = 1;
+int id[MAX_V] = { 0, };
+int V, E, u, v, idx = 1, cnt = 0;
 
 int solved(int cur) {
 	id[cur] = idx++;
@@ -33,7 +33,7 @@ int solved(int cur) {
 			int top = s.top();
 			s.pop();
 			scc.push_back(top);
-			finished[top] = FINISHED;
+			finished[top] = FINISHED;			
 			if (top == cur) break;
 		}
 
@@ -51,16 +51,16 @@ int main(void) {
 	cout.tie(NULL);
 
 	cin >> V >> E;
-
+	
 	for (int i = 0; i < E; i++) {
 		cin >> u >> v;
 
 		edge[u].push_back(v);
 	}
 
-	for (int i = 1; i <= V; i++) if (!id[i]) solved(i);	
-		
+	for (int i = 1; i <= V; i++) if(!id[i]) solved(i);
 	sort(res.begin(), res.end());
+
 	cout << cnt << "\n";
 	for (auto cur : res) {
 		for (auto x : cur) {
--- cpp/4196.cpp
+++ cpp/4196.cpp
@@ -1,13 +1,71 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <stack>
 
 using namespace std;
+
+#define MAX_N 100001
+#define FINISHED true
+
+vector<int> edge[MAX_N];
+stack<int> s;
+int id[MAX_N] = { 0, };
+bool finished[MAX_N] = { 0, };
+int T, N, M, u, v, res = 0, idx = 1;
+
+void init() {
+	res = 0, idx = 1;
+	memset(id, 0, sizeof(id));
+	memset(finished, 0, sizeof(finished));
+	for (int i = 1; i <= N; i++) edge[i].clear();
+}
+
+int solved(int cur) {
+	id[cur] = idx++;
+	s.push(cur);
+
+	int ret = id[cur];
+
+	for (auto nxt : edge[cur]) {
+		if (!id[nxt]) ret = min(ret, solved(nxt));
+		else if (!finished) ret = min(ret, id[nxt]);
+	}
+
+	if (ret == id[cur]) {
+		while (1) {
+			int top = s.top();
+			s.pop();
+			finished[top] = FINISHED;
+			if (top == cur) break;
+		}
+		res++;
+	}
+
+	return ret;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	cin >> T;
+
+	while (T--) {
+		cin >> N >> M;
+		res = N;
+		for (int i = 0; i < M; i++) {
+			cin >> u >> v;
+
+			edge[u].push_back(v);
+		}
+
+		for (int i = 1; i <= N; i++) if (!id[i]) solved(i);
+
+		cout << res << "\n";
+		init();
+	}
 
 	return 0;
 }
