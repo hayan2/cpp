a79eb7014debf3237b5a41db198c58bc815c5952
hayan2 <uhayan2@gmail.com>
2025-02-18 13:31:12 +0900

    0218-1

--- cpp/4196-1.cpp
+++ cpp/4196-1.cpp
@@ -4,9 +4,22 @@
 
 using namespace std;
 
-#define MAX_V 100001
+#define VISITED 1
 
-int V, E, T;
+int V, E, T, u, v;
+
+void dfs(vector<vector<int>>& edge, vector<int>& st, vector<bool>& visited, int cur) {
+	if (visited[cur]) return;
+	visited[cur] = VISITED;
+	for (auto nxt : edge[cur]) dfs(edge, st, visited, nxt);
+	st.push_back(cur);
+}
+
+void solved(vector<vector<int>>& edge, vector<bool>& visited, int cur) {
+	if (visited[cur]) return;
+	visited[cur] = VISITED;
+	for (auto nxt : edge[cur]) solved(edge, visited, nxt);
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -18,9 +31,29 @@ int main(void) {
 	while (T--) {
 		cin >> V >> E;
 
-		vector<int> edge[MAX_V];
+		vector<vector<int>> edge(V + 1);
+		vector<bool> visited(V + 1);
+		vector<int> st;		
 
-		for ()
+		for (int i = 0; i < E; i++) {
+			cin >> u >> v;
+
+			edge[u].push_back(v);
+		}
+
+		for (int i = 1; i <= V; i++) if (!visited[i]) dfs(edge, st, visited, i);
+		reverse(st.begin(), st.end());
+		fill(visited.begin(), visited.end(), false);
+
+		int res = 0;
+		for (auto x : st) {
+			if (!visited[x]) {
+				res++;
+				solved(edge, visited, x);
+			}
+		}
+
+		cout << res << "\n";
 	}
 
 	return 0;
