b91f8de121493da6dfc27b29b7c7b5f42e9e433e
hayan2 <uhayan2@gmail.com>
2025-03-09 09:58:18 +0900

    0309

--- cpp/2357.cpp
+++ cpp/2357.cpp
@@ -6,16 +6,47 @@
 using namespace std;
 
 #define ull unsigned long long
+#define INF 1e9
 
-vector<ull> smax;
-vector<ull> smin;
-int N, M;
+vector<int> smax, smin, cache;
+int N, M, height, x, s, e;
 
 void init() {
-	int x = ceil(log2(N));
-	int height = (1 << (x + 1));
+	x = ceil(log2(N));
+	height = (1 << (x + 1));
 	smax.resize(height);
 	smin.resize(height);
+	cache.resize(N + 1);
+}
+
+int getMaxSegmentTree(int low, int high, int idx) {
+	if (low == high) return smax[idx] = cache[low];
+
+	int mid = (low + high) / 2;
+	return smax[idx] = max(getMaxSegmentTree(low, mid, idx * 2), getMaxSegmentTree(mid + 1, high, idx * 2 + 1));
+}
+
+int getMinSegmentTree(int low, int high, int idx) {
+	if (low == high) return smin[idx] = cache[low];
+
+	int mid = (low + high) / 2;
+	return smin[idx] = min(getMinSegmentTree(low, mid, idx * 2), getMinSegmentTree(mid + 1, high, idx * 2 + 1));
+}
+
+int getMaximum(int low, int high, int idx) {
+	if (s > high || e < low) return -INF;
+	else if (s <= low && high <= e) return smax[idx];
+
+	int mid = (low + high) / 2;
+	return max(getMaximum(low, mid, idx * 2), getMaximum(mid + 1, high, idx * 2 + 1));
+}
+
+int getMinimum(int low, int high, int idx) {
+	if (s > high || e < low) return INF;
+	else if (s <= low && high <= e) return smin[idx];
+
+	int mid = (low + high) / 2;
+	return min(getMinimum(low, mid, idx * 2), getMinimum(mid + 1, high, idx * 2 + 1));
 }
 
 int main(void) {
@@ -27,7 +58,16 @@ int main(void) {
 
 	init();
 
+	for (int i = 0; i < N; i++) cin >> cache[i];
 
+	getMaxSegmentTree(0, N - 1, 1);
+	getMinSegmentTree(0, N - 1, 1);
+
+	while (M--) {
+		cin >> s >> e;
+		s--, e--;
+		cout << getMinimum(0, N - 1, 1) << " " << getMaximum(0, N - 1, 1) << "\n";
+	}
 
 	return 0;
 }
