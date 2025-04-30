e126d4ea8e413d36b8a6bd2d939f84a717b95383
hayan2 <uhayan2@gmail.com>
2025-04-30 11:00:00 +0000

    0429

---
+++ cpp/2494.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int MAX_SIZE = 10001;
+
+string str, cache;
+int dp[MAX_SIZE][10], res[MAX_SIZE][10], N;
+
+int solved(int n, int s) {
+	if (n == N) return 0;
+	if (dp[n][s] != -1) return dp[n][s];
+
+	int x = ((str[n] - '0' + s) % 10 - (cache[n] - '0') + 10) % 10;
+	int l = solved(n + 1, s) + x;
+	int r = solved(n + 1, (s + 10 - x) % 10) + 10 - x;
+
+	if (l < r) dp[n][s] = l, res[n][s] = -x;
+	else dp[n][s] = r, res[n][s] = 10 - x;
+	return dp[n][s];
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> str >> cache;
+	memset(dp, -1, sizeof dp);
+
+	cout << solved(0, 0) << "\n";
+
+	int s = 0;
+	for (int i = 0; i < N; i++) {
+		cout << i + 1 << " " << res[i][s] << "\n";
+		if (res[i][s] > -1) s = (s + res[i][s]) % 10;
+	}
+
+	return 0;
+}
