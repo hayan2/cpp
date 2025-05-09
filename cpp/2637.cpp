1da21c61f926f4e7e55113230309a9aaed4e5be1
hayan2 <uhayan2@gmail.com>
2025-05-09 23:32:35 +0900

    0508

---
+++ cpp/2637.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+using pii = pair<int, int>;
+
+vector<pii> cache;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	return 0;
+}
