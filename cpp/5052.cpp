8de55db9c930c9e23baef0bd01c89836813a7ba5
hayan2 <uhayan2@gmail.com>
2025-05-12 11:30:59 +0900

    0512

---
+++ cpp/5052.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Trie {
+
+};
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	return 0;
+}
