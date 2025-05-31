d7269c7fa12112957ad0871ba3686ea9d69c8ba2
hayan2 <uhayan2@gmail.com>
2025-05-31 23:40:16 +0900

    0531-1

---
+++ cpp/address.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void win() {
+	printf("YOU WIN !\n");
+}
+
+int main(void) {
+	char buf[8];
+	printf("address : %p\n", &win);
+
+	read(0, buf, 8);
+
+	printf("buf : %x\n", buf);
+
+	return 0;
+}
