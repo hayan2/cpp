dc3717e51d4c3b838ac07db3ff06003f02e24232
hayan2 <uhayan2@gmail.com>
2025-06-03 11:00:00 +0000

    0531

--- cpp/1091.cpp
+++ cpp/1091.cpp
@@ -7,12 +7,12 @@ using namespace std;
 const int MAX_SIZE = 51;
 
 set<vector<int>> s;
-vector<int> P, S, cache;
+vector<int> P, S, words;
 int N, cnt = 0;
 
 bool cmp() {
 	for (int i = 0; i < N; i++) 
-		if (P[cache[i]] != (i % 3)) return false;
+		if (P[words[i]] != (i % 3)) return false;
 	return true;
 }
 
@@ -24,14 +24,14 @@ void shuffle() {
 
 	vector<int> tmp(N + 1);
 	for (int i = 0; i < N; i++)
-		tmp[S[i]] = cache[i];
-	cache = tmp;
+		tmp[S[i]] = words[i];
+	words = tmp;
 
-	if (s.find(cache) != s.end()) {
+	if (s.find(words) != s.end()) {
 		cout << -1;
 		return;
 	}
-	s.insert(cache);
+	s.insert(words);
 	cnt++;
 
 	shuffle();
@@ -45,9 +45,9 @@ int main(void) {
 	cin >> N;
 	P.resize(N + 1);
 	S.resize(N + 1);
-	cache.resize(N + 1);
+	words.resize(N + 1);
 	for (int i = 0; i < N; i++) cin >> P[i];
-	for (int i = 0; i < N; i++) { cin >> S[i]; cache[i] = i; }
+	for (int i = 0; i < N; i++) { cin >> S[i]; words[i] = i; }
 
 	shuffle();
 
---
+++ cpp/1099.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+const int MAX_SIZE = 51;
+const int INF = 1e9 + 7;
+
+string str, words[MAX_SIZE];
+int dp[MAX_SIZE], cache[MAX_SIZE][MAX_SIZE], N;
+
+int getCost(int s, int idx) {
+	int& ret = cache[s][idx];
+	if (ret != -1) return ret;
+
+	string w = words[idx];
+	if (s + w.size() > str.size()) return ret = INF;
+
+	ret = 0;
+	vector<int> x(26);
+	for (int i = 0; i < w.size(); i++) {
+		int sc = str[s + i] - 'a';
+		int wc = w[i] - 'a';
+
+		x[sc]--;
+		x[wc]++;
+		if (sc != wc) ret++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (x[i] != 0) return ret = INF;
+	}
+	return ret;
+}
+
+int solved(int s) {
+	if (s >= str.size()) return 0;
+
+	int& ret = dp[s];
+	if (ret != -1) return ret;
+
+	ret = INF;
+	for (int i = 0; i < N; i++) {
+		int cost = getCost(s, i);
+		if (cost != INF)ret = min(ret, cost + solved(s + words[i].size()));
+	}
+
+	return ret;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	memset(dp, -1, sizeof dp);
+	memset(cache, -1, sizeof cache);
+
+	cin >> str;
+	cin >> N;
+	for (int i = 0; i < N; i++) 
+		cin >> words[i];
+	
+	int res = solved(0);
+	cout << ((res == INF) ? -1 : res);
+
+	return 0;
+}
