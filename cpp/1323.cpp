1a650b8a69b0e23f429bc55a03333b45ec709aae
hayan2 <uhayan2@gmail.com>
2025-06-24 11:00:00 +0000

    0625

---
+++ cpp/1323.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll N, K;
+	cin >> N >> K;
+
+	string str = to_string(N);
+	int d = str.length();
+
+	ll sft = 1;
+	for (int i = 0; i < d; i++) sft = (sft * 10) % K;
+
+	ll n = N % K;
+	vector<bool> visited(K, false);
+	int cnt = 1;
+	ll cur = n;
+
+	while (cnt <= K) {
+		if (!cur) {
+			cout << cnt;
+			return 0;
+		}
+		if (visited[cur]) {
+			cout << -1;
+			return 0;
+		}
+		visited[cur] = true;
+		cur = (cur * sft + n) % K;
+		cnt++;
+	}
+
+	return 0;
+}
