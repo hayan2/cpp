1c707c7a01e14a86079d4903dfff05a7e67f2161
hayan2 <uhayan2@gmail.com>
2025-08-17 14:55:10 +0900

    0817

---
+++ cpp/1094.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int X, res = 0;
+
+	cin >> X;
+
+	while (X) {
+		if (X & 1) res++;
+		X >>= 1;
+	}
+
+	cout << res;
+
+	return 0;
+}
