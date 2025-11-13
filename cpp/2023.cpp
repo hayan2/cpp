652f91f2f9fa26f24083b57e7337e0592b8ee168
hayan2 <uhayan2@gmail.com>
2025-11-14 00:04:41 +0900

    1114

---
+++ cpp/2023.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int INF = 1e9 + 7;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	return 0;
+}
