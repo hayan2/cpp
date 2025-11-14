3b95df7ae11ce0fc1ab348dfb000705ad5265491
hayan2 <uhayan2@gmail.com>
2025-11-14 20:36:49 +0900

    1115

---
+++ cpp/2016.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int INF = 1e9 + 7;
+const int P = 5;
+const int TAEHYEON = 0;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		vector<vector<int>> man(P, vector<int>(P));
+		vector<vector<int>> woman(P, vector<int>(P));
+		
+		for (int i = 1; i < P; i++) {
+			for (int j = 0; j < P; j++) {
+
+			}
+		}
+	}
+
+	return 0;
+}
--- cpp/2023.cpp
+++ cpp/2023.cpp
@@ -12,12 +12,39 @@ using pii = pair<int, int>;
 
 const int INF = 1e9 + 7;
 
+int prime[] = { 2, 3, 5, 7 };
+int N;
+
+bool isPrime(int n) {
+	if (n <= 1 || !(n % 2)) return false;	
+	if (n == 2) return true;	
+	for (int i = 3; i * i <= n; i += 2) {
+		if (!(n % i)) return false;
+	}
+
+	return true;
+}
+
+void solve(int n, int d) {
+	if (!isPrime(n) && n != 2) return;
+	if (d == N) {
+		cout << n << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= 9; i += 2) {
+		solve(n * 10 + i, d + 1);
+	}
+}
+
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	cin >> N;
 
+	for (int i = 0; i < 4; i++) solve(prime[i], 1);
 
 	return 0;
 }
